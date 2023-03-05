d7901fd62a6efa0e551249e71ab5631ef3eb58c8
JustAdamCain <03adamcain@gmail.com>
2023-03-05 00:40:22 +0000

    a2

--- COMP281 - Principles of C/A2/assignment2_1.c
+++ COMP281 - Principles of C/A2/assignment2_1.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
 
-void compress(){
-    char* line;
-    while (scanf("%s",line) != EOF)
+#define BUFFER_SIZE;
+
+void output(char character, int count){
+    
+    printf("%c%c%c%d*", character, character, character, count);
+}
+
+void compress()
+{
+    int c, count = 0;
+    const char EOL = '\n';
+    char prevChar;
+    while ((c = getchar()) != EOF)
     {
-        printf("%s",line);
+        printf("%c",c);
+        // if(c = EOL){
+        //     if(count >= 3){
+        //         printRepeats(prevChar,count);
+        //     }else{
+        //         printf("%c",prevChar);
+        //     }
+        //     count = 0;
+        // }else if(prevChar == c){
+        //     count++;
+        // }
+        // else{
+
+        // }
     }
 }
 
-void expand(){
-
+void expand()
+{
 }
 
 int main()
 {
-    char type; 
-    scanf("%c",type);
-    if(type == 'C'){
-        printf("%s","Compress");
+    int c = getchar();
+    if (c == 'C') {
         compress();
-    }else if(type == 'E'){
-        //printf()
-        expand();
+    } else if (c == 'E') {
+        decompress();
     }
     return 0;
 }
